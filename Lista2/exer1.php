c2f41c44517f139dffd7c08542223c5bdf3666d8
Kamily de Souza Gracia <kamily.gracia@gmail.com>
2024-08-20 20:30:13 -0300

    Exercício 1

---
+++ Lista2/exer1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="container">
+    <h1>Exercício 1</h1>
+    <form action="exer1resp.php" method="POST">
+        <div class="row mt-3">
+            <div class="col">
+                <label for="valor1" class="form-label">Informe o valor 1: </label>
+                <input type="number" name="valor1" id="valor1">
+            </div>
+            <div class="col">
+                <label for="valor2" class="form-label">Informe o valor 2: </label>
+                <input type="number" name="valor2" id="valor2">
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
