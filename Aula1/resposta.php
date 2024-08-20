97e5ab486aeb8cd309651916ef339a0ffbf7988c
Kamily de Souza Gracia <kamily.gracia@gmail.com>
2024-08-20 19:43:13 -0300

    complemento da aula 1

--- Aula1/resposta.php
+++ Aula1/resposta.php
@@ -7,8 +7,12 @@
 </head>
 <body>
     <?php
-        $valor = $_POST["valor"];
-        echo "Valor informado: $valor";
+        $valor = (int) $_POST['valor'];
+        $valor = (float) $_POST['valor'];
+        $valor = (string) $_POST['valor'];
+        //$valor = (bool) $_POST['valor'];
+        echo 'Valor informado: '.$valor; #o "." é o operador de concatenação
+        #echo "Valor informado: $valor";
     ?>
 </body>
 </html>
